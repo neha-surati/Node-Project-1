dc466c85f526e920dc9ba32c2c070e677615d84e
Neha Surati <143536556+neha-surati@users.noreply.github.com>
2024-05-07 11:56:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const http = require('http');
+
+const fs = require('fs');
+
+const port = 8081;
+
+const requestHandler = (req, res) => {
+    let filename = "";
+    switch (req.url) {
+        case '/':
+            filename = "./home.html";
+            break;
+
+        case '/about':
+            filename = "./about.html";
+            break;
+
+        case '/contact':
+            filename = "./contact.html";
+            break;
+
+        case '/blogs':
+            filename = "./blogs.html";
+            break;
+
+    }
+    fs.readFile(filename,(err,result)=>{
+        if(!err){
+              
+            res.end(result);
+        }
+        else{
+            
+            res.end("Error");
+        }
+    })
+
+}
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+    if (err) {
+        console.log("server not start in port");
+        return false;
+    }
+    console.log("server server start in port:- " + port);
+})
